f4958340b3c84832947aebec4269973bc38367c8
Jordi Sabadell <jordisabadell@gmail.com>
2020-03-15 07:54:13 +0100

    Add manifest file

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -20,7 +21,8 @@ module.exports = {
     new MiniCSSExtractPlugin({
       filename: "./styles.css",
     }),
-    new WorkboxPlugin.GenerateSW()
+    new WorkboxPlugin.GenerateSW(),
+    new WebpackPwaManifest()
   ],
   module: {
     rules: [
